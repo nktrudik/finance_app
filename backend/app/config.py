2024619c25f966cdd937651b32f04a123a29f96a
ph0nky <earthphonky@gmail.ru>
2025-12-27 22:48:48 +0300

    Updated backend(Rudik pidoras)

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,9 +3,15 @@
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
-# Корневая директория проекта
+load_dotenv()
+
+# Корневая директория проекта и базовые настройки
 BASE_DIR = Path(__file__).parent.parent  # backend/
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 
 
 # ============= DATA =============
@@ -16,14 +22,15 @@ QDRANT_PATH = DATA_DIR / "qdrant_storage"
 QDRANT_COLLECTION_NAME = "financial_transactions"
 
 
+# ============= БЕЗОПАСНОСТЬ =============
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ALGORITHM = os.getenv("ALGORITHM")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
 # ============= API =============
-API_HOST = "0.0.0.0"
-API_PORT = 8000
-
-ACCESS_TOKEN_EXPIRE_MINUTES = 60*24*7
-ALGORITHM = "HS256"
-SECRET_KEY = "XoTOD9ZM4wrDg57-yrTJ06meUSZErw9-Pc67ZDNk2Jo"
-
+API_HOST = os.getenv("API_HOST")
+API_PORT = int(os.getenv("API_PORT"))
 
 # ============= LLM НАСТРОЙКИ =============
 LLM_MODEL_NAME = "Qwen/Qwen2.5-7B-Instruct"
